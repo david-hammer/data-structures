d900efcd68e810fa627ebb2187ecf1711614388f
david-hammer <dhammer126@gmail.com>
2015-02-04 13:30:17 -0800

    finished pseudoclassical

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,21 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.first = 0;
+  this.last = 0;
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(str){
+  this.storage[this.last++] = str;
+};
+Queue.prototype.dequeue = function() {
+  if (this.last - this.first > 0)
+    return this.storage[this.first++];
+  else
+    return null;
+};
+Queue.prototype.size = function() {
+  return this.last - this.first;
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.currSize = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function(str) {
+  this.storage[this.currSize++] = str;
+};
+
+Stack.prototype.pop = function() {
+  if (this.currSize > 0)
+    return this.storage[--this.currSize];
+  else
+    return null;
+};
+
+Stack.prototype.size = function() {
+  return this.currSize;
+};
 

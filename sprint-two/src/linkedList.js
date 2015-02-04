c546a5f98e1fdccc101dbb2ac07f510ef30b47f1
david-hammer <dhammer126@gmail.com>
2015-02-04 15:20:49 -0800

    finished thru tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,40 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if (!list.head) {
+      list.head = Node(value);
+      list.tail = list.head;
+    } else{
+      for (var n = list.head; n.next !== null; n = n.next)
+        ;
+      n.next = Node(value);
+      list.tail = n.next;
+    }
   };
 
   list.removeHead = function(){
+    if (!list.head)
+      return null;
+    else{
+      var oldHead = list.head;
+      list.head = (oldHead.next) ? oldHead.next : null;
+      if (list.head === null)
+        list.tail = null;
+      return oldHead.value;
+    }
   };
 
   list.contains = function(target){
+    var ret = false;
+    for (var n = list.head; n.next !== null; n = n.next){
+      if (n.value === target){
+        ret = true;
+        break;
+      }
+    }
+    if (list.tail.value === target)
+      ret = true;
+    return ret;
   };
 
   return list;
@@ -26,4 +54,8 @@ var Node = function(value){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+    addToTail: O(N)
+    removeHead: O(1)
+    contains: O(N)
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,3 +1,4 @@
+//binary tree
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
@@ -5,24 +6,36 @@ var Tree = function(value){
   // your code here
   newTree.children = null;  // fix me
 
+  for (var key in treeMethods)
+    newTree[key] = treeMethods[key];
+
   return newTree;
 };
-
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  this.children = this.children || [];
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
-
+    var ret = false;
+    var searchTree = function(tree){
+      if (tree.value === target)
+        ret = true;
+      for (var c = 0; tree.children && c < tree.children.length; c++){
+        searchTree(tree.children[c]);
+      }
+    }
+    searchTree(this);
+    return ret;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  addChild : O(1)
+  contains : O(N)
+
  */

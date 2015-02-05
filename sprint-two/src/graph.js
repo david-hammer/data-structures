05a5ba88158c871c02b48a0db42e5bd4daa17e98
david-hammer <dhammer126@gmail.com>
2015-02-04 16:40:17 -0800

    completed through hashTable (remove function of hash table untested and possibly buggy)

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,45 +1,45 @@
 
 
 var Graph = function(){
-  this.nodes = {}; // maps vertices to edge list
+  this._nodes = {}; // maps vertices to edge list
 };
 
 Graph.prototype.addNode = function(node){
-  if (!this.nodes.hasOwnProperty(node)){
-    this.nodes[node] = {};
+  if (!this._nodes.hasOwnProperty(node)){
+    this._nodes[node] = {};
   }
 };
 
 Graph.prototype.contains = function(node){
-  return this.nodes.hasOwnProperty(node);
+  return this._nodes.hasOwnProperty(node);
 };
 
 Graph.prototype.removeNode = function(node){
-  delete this.nodes[node];
+  delete this._nodes[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
-  return this.nodes[fromNode].hasOwnProperty(toNode);
+  return this._nodes[fromNode].hasOwnProperty(toNode);
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
-  if (!this.nodes.hasOwnProperty(fromNode) ||
-    !this.nodes.hasOwnProperty(toNode))
-    throw "vertex does not exist";
-  this.nodes[fromNode][toNode] = toNode;
-  this.nodes[toNode][fromNode] = fromNode;
+  if (!this._nodes.hasOwnProperty(fromNode) ||
+    !this._nodes.hasOwnProperty(toNode))
+    throw new Error("vertex does not exist");
+  this._nodes[fromNode][toNode] = toNode;
+  this._nodes[toNode][fromNode] = fromNode;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
-  if (!this.nodes.hasOwnProperty(fromNode) ||
-    !this.nodes.hasOwnProperty(toNode))
-    throw "vertex does not exist";
-  delete this.nodes[fromNode][toNode];
-  delete this.nodes[toNode][fromNode];
+  if (!this._nodes.hasOwnProperty(fromNode) ||
+    !this._nodes.hasOwnProperty(toNode))
+    throw new Error("vertex does not exist");
+  delete this._nodes[fromNode][toNode];
+  delete this._nodes[toNode][fromNode];
 };
 
 Graph.prototype.forEachNode = function(cb){
-  for (var node in this.nodes)
+  for (var node in this._nodes)
     cb(node);
 };
 
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,3 +1,4 @@
+//collisions resolved by separate chaining
 var HashTable = function(){
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -5,17 +6,60 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(i);
+
+  if (!bucket)
+    this._storage.set(i, new _Node(k,v));
+  else {
+    for (var n = bucket; bucket.next !== null; n = n.next)
+      ;
+    n.next = new _Node(k,v);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  for (var n = bucket; n; n = n.next)
+  {
+    if (n.key === k)
+      return n.value;
+  }
+  return null;
 
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  var prev = null;
+  for (var n = bucket; n; n = n.next)
+  {
+    if (n.key === k)
+    {
+      if (prev === null && n.next === null) //beginning of list
+        this._storage.set(i, null);
+      else if (prev === null)
+        this._storage.set(i,n.next);
+      else if (n.next === null) // end of list
+        prev.next = null;
+      else                      // middle of list
+        prev.next = n.next;
+
+      return n.value;
+    }
+    prev = n;
+  }
+  return null;
 
 };
 
+var _Node = function(k,v){
+  this.key = k;
+  this.value = v;
+  this.next = null;
+};
 
 
 /*
